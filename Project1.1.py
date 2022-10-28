33110f19ea1ceefc786b750671261a9bda878878
Victor Granados <victorgranadosp@gmail.com>
2022-10-28 17:01:07 +0200

    Finished for part b)

--- Project1.1.py
+++ Project1.1.py
@@ -29,9 +29,10 @@ def main():
             continue
         print("The Monte-Carlo integrator gives:", mc_integral, "with a standard error of", std_error)
 
-    plt.hist(integral, bins=80, density=True, ec='black', lw=0.5, color='dodgerblue')
+    plt.hist(integral, bins=80, density=True, ec='black', lw=0.5, color='dodgerblue', label='Data generated')
 
-    avg, std = norm.fit(integral)
+    # Gaussian fit
+    avg, std = norm.fit(integral)  # The function 'norm.fit' takes simply the mean and standard deviation of the data.
     x_min, x_max = plt.xlim()
     x = np.linspace(x_min, x_max, 100)
     gauss_fit = norm.pdf(x, avg, std)
@@ -48,8 +49,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--a', type=float, default=0, help="Lower limit of the integral")
     parser.add_argument('--b', type=float, default=1, help="Upper limit of the integral")
-    parser.add_argument('--N', type=int, default=1000, help="Number of points")
-    parser.add_argument('--M', type=int, default=1, help='Number of sets of random numbers')
+    parser.add_argument('--N', type=int, default=1000, help="Number of points for a single integration")
+    parser.add_argument('--M', type=int, default=1000, help='Number of sets of random numbers')
     parser.add_argument('--show', default=False, action="store_true", help="Show values for integral and error")
+    parser.add_argument('--sampling', default=False, action='store_true', help='Use importance sampling')
     args = parser.parse_args()
     main()
